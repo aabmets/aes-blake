c3825e821e82ba12388ff75d685fdc863929c8b2
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-05-23 08:42:38 +0300

    Moved debugging tools into debug.py module

--- py_ref/src/utils.py
+++ py_ref/src/utils.py
@@ -8,69 +8,37 @@
 #   
 #   SPDX-License-Identifier: MIT
 #
-import re
-from rich.console import Console
-from .uint import BaseUint
+from .uint import Uint32
 
 
 __all__ = [
-	"to_binary_bytes",
-	"to_binary_string",
-	"to_hex_bytes",
-	"to_hex_string",
-	"pretty_print_binary",
-	"pretty_print_hex",
-	"pretty_print_vector"
+	"bytes_to_uint32_vector",
+	"pkcs7_pad",
+	"pkcs7_unpad"
 ]
 
 
-_console = Console()
+def bytes_to_uint32_vector(data: bytes, size: int) -> list[Uint32]:
+	s_len = len(data)
+	count = 4 - (s_len % 4)
+	data += b'\x00' * count
+	output: list[Uint32] = []
+	for i in range(0, s_len, 4):
+		output.append(Uint32.from_bytes(
+			data=data[i:i + 4],
+			byteorder="little"
+		))
+	while len(output) < size:
+		output.append(Uint32(0))
+	return output
 
 
-def to_binary_bytes(uint: BaseUint) -> list[str]:
-	bit_str = format(uint.value, f"0{uint.bit_count}b")
-	return [bit_str[i:i + 8] for i in range(0, len(bit_str), 8)]
+def pkcs7_pad(data):
+	pad_len = 16 - (len(data) % 16)
+	padding = bytes([pad_len] * pad_len)
+	return data + padding
 
 
-def to_binary_string(uint: BaseUint, sep=' ') -> str:
-	return sep.join(to_binary_bytes(uint))
-
-
-def to_hex_bytes(uint: BaseUint) -> list[str]:
-	out = []
-	for bb_str in to_binary_bytes(uint):
-		out.append(f"{int(bb_str, base=2):02X}")
-	return out
-
-
-def to_hex_string(uint: BaseUint, sep=' ') -> str:
-	return sep.join(to_hex_bytes(uint))
-
-
-def pretty_print_binary(uint: BaseUint, color_0="blue", color_1="red", end='\n') -> None:
-	bb_list = []
-	for bb_str in to_binary_bytes(uint):
-		first_color = color_0 if bb_str.startswith('0') else color_1
-		bb_str = re.sub(r"1(0)", rf"1[{color_0}]\1", bb_str)
-		bb_str = re.sub(r"0(1)", rf"0[{color_1}]\1", bb_str)
-		bb_str = f"[{first_color}]{bb_str}"
-		bb_list.append(bb_str)
-	concat_bb = '  '.join(bb_list)
-	_console.print(concat_bb, end=end)
-
-
-def pretty_print_hex(uint: BaseUint, color="green", end='\n') -> None:
-	concat_hex = to_hex_string(uint)
-	hex_str = f"[{color}]{concat_hex}"
-	_console.print(hex_str, end=end)
-
-
-def pretty_print_vector(vector: list[BaseUint], color="yellow") -> None:
-	for i, v in enumerate(vector):
-		if i % 4 == 0 and i != 0:
-			print()
-		print(f"{i:02}: ", end='')
-		pretty_print_hex(v, color=color, end='')
-		if i not in [3, 7, 11, 15]:
-			print('  ', end='')
-	print()
+def pkcs7_unpad(data):
+	pad_len = data[-1]
+	return data[:-pad_len]
