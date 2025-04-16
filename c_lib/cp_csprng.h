10ab865abd3ef2dc95110b7ebbc5b0fef4a23b19
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-04-16 13:53:27 +0300

    Added cross-platform CSPRNG into c_lib

---
+++ c_lib/cp_csprng.h
@@ -0,0 +1,28 @@
+/*
+ *   Apache License 2.0
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef CROSS_PLATFORM_CSPRNG_H
+#define CROSS_PLATFORM_CSPRNG_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint8_t csprng_read(void);
+void csprng_close(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CROSS_PLATFORM_CSPRNG_H */
