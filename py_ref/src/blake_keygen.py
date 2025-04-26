f99689339afb47c93e2b4abeb09ec021e0550c8a
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-04-26 22:54:57 +0300

    Changed BaseBlake properties to static methods

--- py_ref/src/blake_keygen.py
+++ py_ref/src/blake_keygen.py
@@ -35,26 +35,26 @@ class KDFDomain(Enum):
 class BaseBlake(ABC):
     state: list[BaseUint]
 
-    @property
+    @staticmethod
     @abstractmethod
-    def uint(self) -> t.Type[BaseUint]: ...
+    def uint() -> t.Type[BaseUint]: ...
 
-    @property
+    @staticmethod
     @abstractmethod
-    def rots(self) -> tuple[int, ...]: ...
+    def rots() -> tuple[int, ...]: ...
 
-    @property
+    @staticmethod
     @abstractmethod
-    def ivs(self) -> tuple[int, ...]: ...
+    def ivs() -> tuple[int, ...]: ...
 
     @staticmethod
     @abstractmethod
     def domain_mask(domain: KDFDomain) -> int: ...
 
     def __init__(self, key: bytes, nonce: bytes, context: bytes) -> None:
-        self.key = utils.bytes_to_uint_vector(key, self.uint, v_size=16)
-        self.nonce = utils.bytes_to_uint_vector(nonce, self.uint, v_size=8)
-        self.context = utils.bytes_to_uint_vector(context, self.uint, v_size=16)
+        self.key = utils.bytes_to_uint_vector(key, self.uint(), v_size=16)
+        self.nonce = utils.bytes_to_uint_vector(nonce, self.uint(), v_size=8)
+        self.context = utils.bytes_to_uint_vector(context, self.uint(), v_size=16)
         self.init_state_vector(self.nonce, counter=0, domain=KDFDomain.DIGEST_CTX)
 
     def init_state_vector(self, entropy: list[BaseUint], counter: int, domain: KDFDomain) -> None:
@@ -77,10 +77,11 @@ class BaseBlake(ABC):
         Returns:
             None: The internal state is modified in-place.
         """
+        cls, ivs = self.uint(), self.ivs()
         self.state = []
-        self.state.extend(self.uint(iv) for iv in self.ivs[:4])
+        self.state.extend(cls(iv) for iv in ivs[:4])
         self.state.extend(entropy)
-        self.state.extend(self.uint(iv) for iv in self.ivs[4:])
+        self.state.extend(cls(iv) for iv in ivs[4:])
         ctr = Uint64(counter).to_bytes()
         ctr_low = Uint32.from_bytes(ctr[4:])
         ctr_high = Uint32.from_bytes(ctr[:4])
@@ -165,17 +166,17 @@ class BaseBlake(ABC):
         Returns:
             None: The internal state vector is modified in-place.
         """
-        vec = self.state
+        vec, rots = self.state, self.rots()
         # first mixing
         vec[a] = vec[a] + vec[b] + mx
-        vec[d] = (vec[d] ^ vec[a]) >> self.rots[0]
+        vec[d] = (vec[d] ^ vec[a]) >> rots[0]
         vec[c] = vec[c] + vec[d]
-        vec[b] = (vec[b] ^ vec[c]) >> self.rots[1]
+        vec[b] = (vec[b] ^ vec[c]) >> rots[1]
         # second mixing
         vec[a] = vec[a] + vec[b] + my
-        vec[d] = (vec[d] ^ vec[a]) >> self.rots[2]
+        vec[d] = (vec[d] ^ vec[a]) >> rots[2]
         vec[c] = vec[c] + vec[d]
-        vec[b] = (vec[b] ^ vec[c]) >> self.rots[3]
+        vec[b] = (vec[b] ^ vec[c]) >> rots[3]
 
     @staticmethod
     def permute(m: list[BaseUint]) -> list[BaseUint]:
@@ -208,7 +209,7 @@ class BaseBlake(ABC):
         Returns:
             None: The internal state vector is modified in-place.
         """
-        byte_count = self.uint.bit_count() // 8
+        byte_count = self.uint().bit_count() // 8
         for column in range(4):
             u_ints = [v.to_bytes() for i, v in enumerate(self.state) if i % 4 == column]
             subbed_bytes = [SBox.ENC.value[b] for b in b''.join(u_ints)]
@@ -217,21 +218,21 @@ class BaseBlake(ABC):
                     subbed_bytes[4 * ((row + offset) % 4) + offset]
                     for offset in range(byte_count)
                 ]
-                new_uint = self.uint.from_bytes(new_bytes)
+                new_uint = self.uint().from_bytes(new_bytes)
                 self.state[column + row * 4] = new_uint
 
 
 class Blake32(BaseBlake):
-    @property
-    def uint(self) -> t.Type[Uint32]:
+    @staticmethod
+    def uint() -> t.Type[Uint32]:
         return Uint32
 
-    @property
-    def rots(self) -> tuple[int, ...]:
+    @staticmethod
+    def rots() -> tuple[int, ...]:
         return 16, 12, 8, 7
 
-    @property
-    def ivs(self) -> tuple[int, ...]:
+    @staticmethod
+    def ivs() -> tuple[int, ...]:
         return (  # From BLAKE2s, which in turn took them from SHA-256
             0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A,
             0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
@@ -251,16 +252,16 @@ class Blake32(BaseBlake):
 
 
 class Blake64(BaseBlake):
-    @property
-    def uint(self) -> t.Type[Uint64]:
+    @staticmethod
+    def uint() -> t.Type[Uint64]:
         return Uint64
 
-    @property
-    def rots(self) -> tuple[int, ...]:
+    @staticmethod
+    def rots() -> tuple[int, ...]:
         return 32, 24, 16, 63
 
-    @property
-    def ivs(self) -> tuple[int, ...]:
+    @staticmethod
+    def ivs() -> tuple[int, ...]:
         return (  # From BLAKE2b, which in turn took them from SHA-512
             0x6A09E667F3BCC908, 0xBB67AE8584CAA73B, 0x3C6EF372FE94F82B, 0xA54FF53A5F1D36F1,
             0x510E527FADE682D1, 0x9B05688C2B3E6C1F, 0x1F83D9ABFB41BD6B, 0x5BE0CD19137E2179,
