8495d7475a011e251d9ae7f0ee610a3d45f5132d
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-06-13 11:41:05 +0300

    Moved Blake constants and state vector init functions into a shared module

---
+++ c_lib/blake_keygen/blake_shared.c
@@ -0,0 +1,106 @@
+/*
+ *   Apache License 2.0
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+#include "blake_shared.h"
+
+
+const uint32_t IV32[8] = {
+    0x6A09E667u, 0xBB67AE85u, 0x3C6EF372u, 0xA54FF53Au,
+    0x510E527Fu, 0x9B05688Cu, 0x1F83D9ABu, 0x5BE0CD19u
+};
+
+const uint64_t IV64[8] = {
+    0x6A09E667F3BCC908ULL, 0xBB67AE8584CAA73BULL,
+    0x3C6EF372FE94F82BULL, 0xA54FF53A5F1D36F1ULL,
+    0x510E527FADE682D1ULL, 0x9B05688C2B3E6C1FULL,
+    0x1F83D9ABFB41BD6BULL, 0x5BE0CD19137E2179ULL
+};
+
+
+/*
+ * Initializes the 16-word state matrix for the compression function.
+ * Implements:
+ *   state[0..3]   = IV constants[0..3] (BLAKE2s)
+ *   state[4..7] = entropy[0..3] + low‐32 bits of counter
+ *   state[8..11] = entropy[4..7] + high‐32 bits of counter
+ *   state[12..15] = IV constants[4..7] (BLAKE2s) ⊕ domain mask
+ */
+void init_state_vector32(
+        uint32_t state[16],
+        const uint32_t entropy[8],
+        const uint64_t counter,
+        const KDFDomain domain
+) {
+    const uint32_t ctr_low  = (uint32_t)(counter & 0xFFFFFFFFu);
+    const uint32_t ctr_high = (uint32_t)(counter >> 32 & 0xFFFFFFFFu);
+    const uint32_t d_mask = get_domain_mask32(domain);
+
+    state[0] = IV32[0];
+    state[1] = IV32[1];
+    state[2] = IV32[2];
+    state[3] = IV32[3];
+
+    state[4] = entropy[0] + ctr_low;
+    state[5] = entropy[1] + ctr_low;
+    state[6] = entropy[2] + ctr_low;
+    state[7] = entropy[3] + ctr_low;
+
+    state[8] = entropy[4] + ctr_high;
+    state[9] = entropy[5] + ctr_high;
+    state[10] = entropy[6] + ctr_high;
+    state[11] = entropy[7] + ctr_high;
+
+    state[12] = IV32[4] ^ d_mask;
+    state[13] = IV32[5] ^ d_mask;
+    state[14] = IV32[6] ^ d_mask;
+    state[15] = IV32[7] ^ d_mask;
+}
+
+
+/*
+ * Initializes the 16-word state matrix for the compression function.
+ * Implements:
+ *   state[0..3] = IV constants[0..3] (BLAKE2b)
+ *   state[4..7] = entropy[0..3] + low‐32 bits of counter
+ *   state[8..11] = entropy[4..7] + high‐32 bits of counter
+ *   state[12..15] = IV constants[4..7] (BLAKE2b) ⊕ domain mask
+ */
+void init_state_vector64(
+        uint64_t state[16],
+        const uint64_t entropy[8],
+        const uint64_t counter,
+        const KDFDomain domain
+) {
+    const uint32_t ctr_low  = (uint32_t)(counter & 0xFFFFFFFFu);
+    const uint32_t ctr_high = (uint32_t)(counter >> 32 & 0xFFFFFFFFu);
+    const uint64_t d_mask = get_domain_mask64(domain);
+
+    state[0] = IV64[0];
+    state[1] = IV64[1];
+    state[2] = IV64[2];
+    state[3] = IV64[3];
+
+    state[4] = entropy[0] + ctr_low;
+    state[5] = entropy[1] + ctr_low;
+    state[6] = entropy[2] + ctr_low;
+    state[7] = entropy[3] + ctr_low;
+
+    state[8] = entropy[4] + ctr_high;
+    state[9] = entropy[5] + ctr_high;
+    state[10] = entropy[6] + ctr_high;
+    state[11] = entropy[7] + ctr_high;
+
+    state[12] = IV64[4] ^ d_mask;
+    state[13] = IV64[5] ^ d_mask;
+    state[14] = IV64[6] ^ d_mask;
+    state[15] = IV64[7] ^ d_mask;
+}
---
+++ c_lib/blake_keygen/blake_shared.h
@@ -0,0 +1,81 @@
+/*
+ *   Apache License 2.0
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
+
+
+#ifndef BLAKE_SHARED_H
+#define BLAKE_SHARED_H
+
+#ifdef __cplusplus
+#include <cstdint>
+extern "C" {
+#else
+#include <stdint.h>
+#endif
+
+
+    extern const uint32_t IV32[8];
+    extern const uint64_t IV64[8];
+
+    typedef enum {
+        KDFDomain_CTX = 0,
+        KDFDomain_MSG = 1,
+        KDFDomain_HDR = 2,
+        KDFDomain_CHK = 3
+    } KDFDomain;
+
+    inline uint32_t get_domain_mask32(const KDFDomain domain) {
+        switch (domain) {
+            case KDFDomain_CTX: return 0x00000000u;
+            case KDFDomain_MSG: return 0x00F0000Fu;
+            case KDFDomain_HDR: return 0x0F000F00u;
+            case KDFDomain_CHK: return 0xF00F0000u;
+            default:            return 0x00000000u;
+        }
+    }
+
+    inline uint64_t get_domain_mask64(const KDFDomain domain) {
+        switch (domain) {
+            case KDFDomain_CTX: return 0x0000000000000000ULL;
+            case KDFDomain_MSG: return 0x0000FF00000000FFULL;
+            case KDFDomain_HDR: return 0x00FF000000FF0000ULL;
+            case KDFDomain_CHK: return 0xFF0000FF00000000ULL;
+            default:            return 0x0000000000000000ULL;
+        }
+    }
+
+    inline uint32_t rotr32(const uint32_t x, const uint8_t r) {
+        return x >> r | x << (32 - r);
+    }
+
+    inline uint64_t rotr64(const uint64_t x, const uint8_t r) {
+        return x >> r | x << (64 - r);
+    }
+
+    void init_state_vector32(
+        uint32_t state[16],
+        const uint32_t entropy[8],
+        uint64_t counter,
+        KDFDomain domain
+    );
+
+    void init_state_vector64(
+        uint64_t state[16],
+        const uint64_t entropy[8],
+        uint64_t counter,
+        KDFDomain domain
+    );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //BLAKE_SHARED_H
