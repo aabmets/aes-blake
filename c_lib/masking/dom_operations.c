78d87424c579414b35cd67ada65544934e2b17e4
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-06-18 22:36:21 +0300

    Reworked boolean 'and' DOM operation into a macro template

---
+++ c_lib/masking/dom_operations.c
@@ -0,0 +1,70 @@
+/*
+ *   Apache License 2.0
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <stdint.h>
+#include "csprng.h"
+#include "masking.h"
+
+
+/*
+ *   Parametrized preprocessor macro template for all operations functions.
+ */
+#ifndef DOM_OPERATION_FUNCTIONS
+#define DOM_OPERATION_FUNCTIONS(TYPE, FN_SUFFIX)                                \
+                                                                                \
+/*   Implements the DOM-indep secure multiplication/AND   */                    \
+/*   of 2-nd order shares, as described by Gross et al.   */                    \
+/*   in “Domain-Oriented Masking” (CHES 2016).            */                    \
+void dom_bool_and_##FN_SUFFIX(                                                  \
+        const masked_##TYPE* mv_a,                                              \
+        const masked_##TYPE* mv_b,                                              \
+        masked_##TYPE* mv_out                                                   \
+) {                                                                             \
+    /* --- Generate randomness --- */                                           \
+    TYPE rand[N_SHARES];                                                        \
+    csprng_read_array((uint8_t*)rand, sizeof(rand));                            \
+    const TYPE r01 = rand[0], r02 = rand[1], r12 = rand[2];                     \
+                                                                                \
+    /* --- Load shares into local variables --- */                              \
+    const TYPE* x = mv_a->shares;                                               \
+    const TYPE* y = mv_b->shares;                                               \
+    const TYPE x0 = x[0], x1 = x[1], x2 = x[2];                                 \
+    const TYPE y0 = y[0], y1 = y[1], y2 = y[2];                                 \
+                                                                                \
+    /* --- Resharing phase (computation) --- */                                 \
+    const TYPE p01_masked = x0 & y1 ^ r01;                                      \
+    const TYPE p10_masked = x1 & y0 ^ r01;                                      \
+                                                                                \
+    const TYPE p02_masked = x0 & y2 ^ r02;                                      \
+    const TYPE p20_masked = x2 & y0 ^ r02;                                      \
+                                                                                \
+    const TYPE p12_masked = x1 & y2 ^ r12;                                      \
+    const TYPE p21_masked = x2 & y1 ^ r12;                                      \
+                                                                                \
+    /* --- Integration phase --- */                                             \
+    TYPE* out = mv_out->shares;                                                 \
+    out[0] = x0 & y0 ^ p01_masked ^ p02_masked;                                 \
+    out[1] = x1 & y1 ^ p10_masked ^ p12_masked;                                 \
+    out[2] = x2 & y2 ^ p20_masked ^ p21_masked;                                 \
+                                                                                \
+    /* --- Compiler memory barrier --- */                                       \
+    asm volatile ("" ::: "memory");                                             \
+}
+
+#endif //DOM_OPERATION_FUNCTIONS
+
+
+/*
+ *   Create operations functions for all supported types.
+ */
+DOM_OPERATION_FUNCTIONS(uint8_t, u8)
+DOM_OPERATION_FUNCTIONS(uint32_t, u32)
+DOM_OPERATION_FUNCTIONS(uint64_t, u64)
