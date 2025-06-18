fb7677748ecd23009aaf7bba040a5c213435bd81
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-06-18 20:12:38 +0300

    Reworked the impl of DOM utility functions

--- c_lib/masking/dom_utils.c
+++ c_lib/masking/dom_utils.c
@@ -9,83 +9,103 @@
  *   SPDX-License-Identifier: Apache-2.0
  */
 
+#include <stdlib.h>
 #include <stdint.h>
 #include "csprng.h"
-#include "masking.h"
+#include "dom_types.h"
 
 
-/* --- 8-BIT FUNCTIONS --- */
+#ifndef ALIGNED_ALLOC_FUNC
+#define ALIGNED_ALLOC_FUNC
+    #if defined(_WIN32)
+        #include <malloc.h>
+        #define aligned_alloc(alignment, size) _aligned_malloc(size, alignment)
+        #define aligned_free(ptr) _aligned_free(ptr)
+    #else
+        #define aligned_free(ptr) free(ptr)
+    #endif
+#endif //ALIGNED_ALLOC_FUNC
 
-void dom_mask8(const uint8_t x, uint8_t s[N_SHARES]) {
-    s[1] = csprng_read();
-    s[2] = csprng_read();
-    s[0] = x ^ s[1] ^ s[2];
+
+/*
+ *   We define a parametrized preprocessor macro template for all utility functions.
+ */
+#ifndef DOM_UTILITY_FUNCTIONS
+#define DOM_UTILITY_FUNCTIONS(TYPE, FN_SUFFIX, BIT_LENGTH)                      \
+                                                                                \
+masked_##TYPE* dom_mask_##FN_SUFFIX(                                            \
+        const TYPE value,                                                       \
+        const domain_t domain                                                   \
+) {                                                                             \
+    const size_t alignment = sizeof(void*);                                     \
+    const size_t offset = (sizeof(masked_##TYPE) + alignment - 1);              \
+    const size_t aligned_bytes = offset & ~(alignment - 1);                     \
+                                                                                \
+    masked_##TYPE* mv = aligned_alloc(alignment, aligned_bytes);                \
+    if (!mv) return NULL;                                                       \
+                                                                                \
+    mv->bit_length = BIT_LENGTH;                                                \
+    mv->share_count = N_SHARES;                                                 \
+    mv->domain = domain;                                                        \
+                                                                                \
+    TYPE rval[N_SHARES - 1];                                                    \
+    csprng_read_array((uint8_t*)&rval, sizeof(rval));                           \
+                                                                                \
+    if (domain == DOMAIN_BOOLEAN) {                                             \
+        mv->shares[0] = value ^ rval[0] ^ rval[1];                              \
+    } else {  /* DOMAIN_ARITHMETIC */                                           \
+        mv->shares[0] = (TYPE)(value + rval[0] + rval[1]);                      \
+    }                                                                           \
+    mv->shares[1] = rval[0];                                                    \
+    mv->shares[2] = rval[1];                                                    \
+    return mv;                                                                  \
+}                                                                               \
+                                                                                \
+                                                                                \
+TYPE dom_unmask_##FN_SUFFIX(masked_##TYPE* mv) {                                \
+    TYPE result;                                                                \
+    if (mv->domain == DOMAIN_BOOLEAN) {                                         \
+        result = mv->shares[0] ^ mv->shares[1] ^ mv->shares[2];                 \
+    } else {  /* DOMAIN_ARITHMETIC */                                           \
+        result = mv->shares[0] - mv->shares[1] - mv->shares[2];                 \
+    }                                                                           \
+    aligned_free(mv);                                                           \
+    return result;                                                              \
+}                                                                               \
+                                                                                \
+                                                                                \
+void dom_copy_##FN_SUFFIX(                                                      \
+        const masked_##TYPE* mv_src,                                            \
+        masked_##TYPE* mv_tgt                                                   \
+) {                                                                             \
+    mv_tgt->domain = mv_src->domain;                                            \
+    mv_tgt->shares[0] = mv_src->shares[0];                                      \
+    mv_tgt->shares[1] = mv_src->shares[1];                                      \
+    mv_tgt->shares[2] = mv_src->shares[2];                                      \
+}                                                                               \
+                                                                                \
+                                                                                \
+void dom_refresh_mask_##FN_SUFFIX(masked_##TYPE* mv) {                          \
+    TYPE rval[N_SHARES - 1];                                                    \
+    csprng_read_array((uint8_t*)&rval, sizeof(rval));                           \
+                                                                                \
+    if (mv->domain == DOMAIN_BOOLEAN) {                                         \
+        mv->shares[0] ^= rval[0] ^ rval[1];                                     \
+        mv->shares[1] ^= rval[0];                                               \
+        mv->shares[2] ^= rval[1];                                               \
+    } else { /* DOMAIN_ARITHMETIC */                                            \
+        mv->shares[0] += rval[0] + rval[1];                                     \
+        mv->shares[1] += rval[0];                                               \
+        mv->shares[2] += rval[1];                                               \
+    }                                                                           \
 }
 
-uint8_t dom_unmask8(const uint8_t s[3]) {
-    return s[0] ^ s[1] ^ s[2];
-}
-
-void dom_refresh_mask8(uint8_t s[N_SHARES]) {
-    const uint8_t r1 = csprng_read();
-    const uint8_t r2 = csprng_read();
-    s[0] ^= r1;
-    s[1] ^= r2;
-    s[2] ^= r1 ^ r2;
-}
-
-void dom_copy8(const uint8_t x[N_SHARES], uint8_t s[N_SHARES]) {
-    s[0] = x[0]; s[1] = x[1]; s[2] = x[2];
-}
+#endif //DOM_UTILITY_FUNCTIONS
 
 
-/* --- 32-BIT FUNCTIONS --- */
-
-void dom_mask32(const uint32_t x, uint32_t s[N_SHARES]) {
-    csprng_read_array((uint8_t*)&s[1], sizeof(uint32_t));
-    csprng_read_array((uint8_t*)&s[2], sizeof(uint32_t));
-    s[0] = x ^ s[1] ^ s[2];
-}
-
-uint32_t dom_unmask32(const uint32_t s[3]) {
-    return s[0] ^ s[1] ^ s[2];
-}
-
-void dom_refresh_mask32(uint32_t s[N_SHARES]) {
-    uint32_t r1, r2;
-    csprng_read_array((uint8_t*)&r1, sizeof(uint32_t));
-    csprng_read_array((uint8_t*)&r2, sizeof(uint32_t));
-    s[0] ^= r1;
-    s[1] ^= r2;
-    s[2] ^= r1 ^ r2;
-}
-
-void dom_copy32(const uint32_t x[N_SHARES], uint32_t s[N_SHARES]) {
-    s[0] = x[0]; s[1] = x[1]; s[2] = x[2];
-}
-
-
-/* --- 64-BIT FUNCTIONS --- */
-
-void dom_mask64(const uint64_t x, uint64_t s[N_SHARES]) {
-    csprng_read_array((uint8_t*)&s[1], sizeof(uint64_t));
-    csprng_read_array((uint8_t*)&s[2], sizeof(uint64_t));
-    s[0] = x ^ s[1] ^ s[2];
-}
-
-uint64_t dom_unmask64(const uint64_t s[3]) {
-    return s[0] ^ s[1] ^ s[2];
-}
-
-void dom_refresh_mask64(uint64_t s[N_SHARES]) {
-    uint64_t r1, r2;
-    csprng_read_array((uint8_t*)&r1, sizeof(uint64_t));
-    csprng_read_array((uint8_t*)&r2, sizeof(uint64_t));
-    s[0] ^= r1;
-    s[1] ^= r2;
-    s[2] ^= r1 ^ r2;
-}
-
-void dom_copy64(const uint64_t x[N_SHARES], uint64_t s[N_SHARES]) {
-    s[0] = x[0]; s[1] = x[1]; s[2] = x[2];
-}
+/*
+ *   We use the macro to create utility functions for all supported types.
+ */
+DOM_UTILITY_FUNCTIONS(uint8_t, u8, BIT_LENGTH_8)
+DOM_UTILITY_FUNCTIONS(uint32_t, u32, BIT_LENGTH_32)
+DOM_UTILITY_FUNCTIONS(uint64_t, u64, BIT_LENGTH_64)
