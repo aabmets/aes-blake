d78b6ec6095e6e107e7bde63ad119fd6433d6011
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-07-09 10:41:21 +0300

    Optimized dom_conv_btoa logic and memory safety

--- c_lib/masking/dom_converters.c
+++ c_lib/masking/dom_converters.c
@@ -11,6 +11,7 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "csprng.h"
 #include "masking.h"
@@ -26,20 +27,19 @@ static inline TYPE psi_##FN_SUFFIX(TYPE masked, TYPE mask) {                    
 }                                                                               \
                                                                                 \
 static TYPE *convert_##FN_SUFFIX(const TYPE *x, uint8_t n_plus1) {              \
-    uint8_t n = n_plus1 - 1;                                                    \
+    const uint8_t n = n_plus1 - 1;                                              \
     if (n == 1) {                                                               \
         TYPE *out = (TYPE *)malloc(sizeof(TYPE));                               \
-        out[0] = x[0] ^ x[1];                                                   \
+        if (!out) return NULL;                                                  \
+        *out = x[0] ^ x[1];                                                     \
         return out;                                                             \
     }                                                                           \
                                                                                 \
-    TYPE *rnd = (TYPE *)malloc(n * sizeof(TYPE));                               \
+    TYPE rnd[n];                                                                \
     csprng_read_array((uint8_t *)rnd, n * sizeof(TYPE));                        \
                                                                                 \
-    TYPE *x_mut = (TYPE *)malloc(n_plus1 * sizeof(TYPE));                       \
-    for (uint8_t i = 0; i < n_plus1; ++i) {                                     \
-        x_mut[i] = x[i];                                                        \
-    }                                                                           \
+    TYPE x_mut[n_plus1];                                                        \
+    memcpy(x_mut, x, n_plus1 * sizeof(TYPE));                                   \
                                                                                 \
     for (uint8_t i = 1; i < n_plus1; ++i) {                                     \
         TYPE r = rnd[i - 1];                                                    \
@@ -47,28 +47,38 @@ static TYPE *convert_##FN_SUFFIX(const TYPE *x, uint8_t n_plus1) {              
         x_mut[0] ^= r;                                                          \
     }                                                                           \
                                                                                 \
-    TYPE *y = (TYPE *)malloc(n * sizeof(TYPE));                                 \
+    TYPE y[n];                                                                  \
     TYPE first_term = ((n - 1) & 1U) ? x_mut[0] : (TYPE)0;                      \
     y[0] = first_term ^ psi_##FN_SUFFIX(x_mut[0], x_mut[1]);                    \
     for (uint8_t i = 1; i < n; ++i) {                                           \
         y[i] = psi_##FN_SUFFIX(x_mut[0], x_mut[i + 1]);                         \
     }                                                                           \
                                                                                 \
-    TYPE *first  = convert_##FN_SUFFIX(&x_mut[1], n);                           \
+    TYPE *first = convert_##FN_SUFFIX(&x_mut[1], n);                            \
     TYPE *second = convert_##FN_SUFFIX(y, n);                                   \
+    if (!first || !second) {                                                    \
+        free(first);                                                            \
+        free(second);                                                           \
+        return NULL;                                                            \
+    }                                                                           \
                                                                                 \
+    size_t buf_size = (size_t)(n - 1) * sizeof(TYPE);                           \
     TYPE *out = (TYPE *)malloc(n * sizeof(TYPE));                               \
+    if (!out) {                                                                 \
+        secure_memzero(first, buf_size);                                        \
+        secure_memzero(second, buf_size);                                       \
+        free(first);                                                            \
+        free(second);                                                           \
+        return NULL;                                                            \
+    }                                                                           \
     for (uint8_t i = 0; i < n - 2; ++i) {                                       \
         out[i] = first[i] + second[i];                                          \
     }                                                                           \
-    uint8_t n_1 = n - 1;                                                        \
-    uint8_t n_2 = n - 2;                                                        \
-    out[n_2] = first[n_2];                                                      \
-    out[n_1] = second[n_2];                                                     \
+    out[n - 2] = first[n - 2];                                                  \
+    out[n - 1] = second[n - 2];                                                 \
                                                                                 \
-    free(rnd);                                                                  \
-    free(x_mut);                                                                \
-    free(y);                                                                    \
+    secure_memzero(first, buf_size);                                            \
+    secure_memzero(second, buf_size);                                           \
     free(first);                                                                \
     free(second);                                                               \
     return out;                                                                 \
@@ -88,23 +98,35 @@ DOM_BTOA_HELPERS(TYPE, FN_SUFFIX)                                               
 /*   "Improved High-Order Conversion From Boolean to Arithmetic Masking"   */   \
 /*   Link: https://eprint.iacr.org/2018/328.pdf                            */   \
 void dom_conv_btoa_##FN_SUFFIX(masked_##TYPE *mv) {                             \
-    if (mv->domain == DOMAIN_ARITHMETIC) return;                                \
+    if (mv->domain == DOMAIN_ARITHMETIC)                                        \
+        return;                                                                 \
+                                                                                \
     uint8_t sc = mv->share_count;                                               \
     uint8_t sc_extra = sc + 1;                                                  \
-    TYPE *shares = mv->shares;                                                  \
                                                                                 \
     TYPE *tmp = (TYPE *)malloc(sc_extra * sizeof(TYPE));                        \
+    if (!tmp)                                                                   \
+        return;                                                                 \
+                                                                                \
+    TYPE *shares = mv->shares;                                                  \
     for (uint8_t i = 0; i < sc; ++i) {                                          \
         tmp[i] = shares[i];                                                     \
     }                                                                           \
     tmp[sc] = (TYPE)0;                                                          \
                                                                                 \
     TYPE *new_shares = convert_##FN_SUFFIX(tmp, sc_extra);                      \
+    if (!new_shares) {                                                          \
+        secure_memzero(tmp, sc_extra * sizeof(TYPE));                           \
+        free(tmp);                                                              \
+        return;                                                                 \
+    }                                                                           \
     for (uint8_t i = 0; i < sc; ++i) {                                          \
         mv->shares[i] = new_shares[i];                                          \
     }                                                                           \
     mv->domain = DOMAIN_ARITHMETIC;                                             \
                                                                                 \
+    secure_memzero(tmp, sc_extra * sizeof(TYPE));                               \
+    secure_memzero(new_shares, sc * sizeof(TYPE));                              \
     free(tmp);                                                                  \
     free(new_shares);                                                           \
     asm volatile ("" ::: "memory");                                             \
