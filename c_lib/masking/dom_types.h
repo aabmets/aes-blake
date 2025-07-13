7a0aa76d39c30e46f1eb96002fc707abc9d3a466
Mattias Aabmets <mattias.aabmets@gmail.com>
2025-07-13 19:09:08 +0300

    Fixed license header in dom_types.h

--- c_lib/masking/dom_types.h
+++ c_lib/masking/dom_types.h
@@ -1,6 +1,13 @@
-//
-// Created by MattiasAabmets on 18.06.2025.
-//
+/*
+ *   Apache License 2.0
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: Apache-2.0
+ */
 
 #ifndef DOM_TYPES_H
 #define DOM_TYPES_H
